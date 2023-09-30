b76ad34b7a16463f2daf5bee1ac5f51ad491d629
afsmaira <andre.smaira@alumni.usp.br>
2023-09-30 15:40:34 -0300

    Generalização para executar baseado na lista de estacionamentos disponível no arquivo de configuração

--- busca.py
+++ busca.py
@@ -3,16 +3,20 @@ import sys
 from estacionamentos import *
 
 if __name__ == '__main__':
-    na = len(sys.argv)
-    assert na >= 5
-    d1, h1, d2, h2 = sys.argv[1:5]
-    # TODO: list of promo codes
-    promo = sys.argv[5] if na > 5 else ''
-    dh1 = d1+' '+h1
-    dh2 = d2+' '+h2
-    lst = list(GRU(dh1, dh2, promo))
-    lst += EconoPark(dh1, dh2)
-    lst += AeroPark(dh1, dh2)
-    print('\n'.join(f'{n}: R$ {p:.2f}'
-                    for p, n in sorted(lst)))
-
+    cfg = configparser.ConfigParser()
+    cfg.optionxform = str
+    cfg.read('config.cfg')
+    dh1 = cfg['Estadia']['inicio']
+    dh2 = cfg['Estadia']['fim']
+    promos = cfg['Cupons']
+    tds = len(sys.argv) > 1 and 'all' in sys.argv
+    lst = []
+    for k, v in cfg['Estacionamentos'].items():
+        if tds or v == 'true':
+            try:
+                promo = promos.get('GRU', '')
+                lst += eval(f'{k}("{dh1}", "{dh2}", "{promo}")')
+            except NameError as err:
+                print(err)
+    print('\n' + '\n'.join(f'{n}: R$ {p:.2f} {c}'
+                           for p, n, c in sorted(lst)))
